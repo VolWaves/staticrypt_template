b28464f3e5e04b70a5f5a6daf2ccb95c8df3ee00
xianii <xianyi.xia@outlook.com>
2023-06-26 15:48:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Volwave staticrypt 加密模板
+
+
+## 安装依赖
+
+```shell
+npm install -g staticrypt
+```
+
+## 加密
+
+```shell
+./scrypt.ps1 待加密目录 文档编号 密码
+```
+
+加密时会在目录下生成`.staticrypt.json`文件，记录了加密盐，使用了相同密码和加密盐的页面在保存密码后跳转时不用重复解密。
+
+如果需要使用不同的加密盐，删除旧的JSON配置文件即可。
---
+++ scrypt.ps1
@@ -0,0 +1,23 @@
+﻿# this file should be saved in UTF8 with BOM
+
+$Usage = "用法: scrypt 待加密目录 文档编号 密码"
+$Err1 = "错误: 第一个参数非有效目录"
+if ($args.count -lt 3) {
+	write-host $Usage
+	exit
+}
+if (-Not (Test-Path $args[0])) {
+	write-host $Err1`n$Usage
+	exit
+}
+staticrypt $args[0] `
+--template-instructions "编号VD$($args[1])" `
+-p $args[2] `
+-t ./volwave_template.html `
+--short -r `
+--template-title "Volwave加密文档" `
+--template-remember "记住密码" `
+--template-error "密码错误" `
+--template-button "解密查看"
+
+write-host "【加密完成】文档编号VD$($args[1])"
